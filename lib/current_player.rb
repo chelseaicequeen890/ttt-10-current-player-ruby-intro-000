a9c31967ff547caccc12af4503b8a4935a3d761d
Alicia Wu <ali.ciawu890@gmail.com>
2019-01-29 16:22:20 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,7 +1,7 @@
 def turn_count(board)
   count = 0
   board.each do |position|
-    if board[position] != " " || board[position] != "" || board[position] != nil
+    if position == "X" || position == "O"
       count += 1
     end
   end
